4dafaa1c1bedc45e3b53a3c9d01815d384a3b3c2
rafa30052003 <91888240+rafa30052003@users.noreply.github.com>
2021-12-13 16:27:30 +0100

    layout 2

---
+++ xml&&xsd/ejercicoEntornos.java
@@ -0,0 +1,23 @@
+public class ejercicoEntornos{
+    public static void main(String[] args) {
+        int boton;
+        System.out.println("escribe boton");
+            switch (boton) {
+                case inicio:
+                System.out.println("inicio");
+                    
+                    break;
+                    case descripcion:
+                    System.out.println("descripcion");
+                        
+                        break;
+                        case solucion:
+                        System.out.println("solucion");
+                            
+                            break;
+            
+                default:
+                    break;
+            }
+    }
+}
